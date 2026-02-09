ac2ce748ca7788c013b95a692c9f7857c75a65c3
Malulika <yonimalul43@gmail.com>
2026-02-09 20:06:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/edittext/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.edittext;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText eT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        eT=(EditText)findViewById(R.id.Et);
+        btn=findViewById(R.id.btn);
+    }
+
+    
